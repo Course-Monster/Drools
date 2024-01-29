44c79a7edade334988a6b8df6df2ef16f6d7ae0a
Guambino <Ivan.Guambe@fnb.co.mz>
2024-01-29 16:28:29 +0200

    Added Source code for gateway

---
+++ Course Material/labs/drools-training-gateway/src/test/java/africa/skunkworks/training/drools/gateway/DroolsTrainingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.skunkworks.training.drools.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsTrainingGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
