0a2e1f85fd6890e33a862891e5a0fc7afdfd7b27
Guambino <Ivan.Guambe@fnb.co.mz>
2024-01-29 18:44:39 +0200

    Service Module added

---
+++ Course Material/labs/drools-training-insurance/drools-training-insurance-service/src/test/java/africa/skunkworks/training/drools/insurance/service/AbstractServiceTest.java
@@ -0,0 +1,8 @@
+package africa.skunkworks.training.drools.insurance.service;
+
+import africa.skunkworks.training.drools.insurance.service.config.ServiceConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {ServiceConfig.class})
+public class AbstractServiceTest {
+}
