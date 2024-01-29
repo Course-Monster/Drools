d819d82d4b4dadf330ba816cda3c750c5ab754d4
Guambino <Ivan.Guambe@fnb.co.mz>
2024-01-29 18:22:06 +0200

    Repository Module added

---
+++ Course Material/labs/drools-training-insurance/drools-training-insurance-repository/src/test/java/africa/skunkworks/training/drools/insurance/repository/AbstractRepositoryTest.java
@@ -0,0 +1,8 @@
+package africa.skunkworks.training.drools.insurance.repository;
+
+import africa.skunkworks.training.drools.insurance.repository.config.RepositoryConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {RepositoryConfig.class})
+public abstract class AbstractRepositoryTest {
+}
