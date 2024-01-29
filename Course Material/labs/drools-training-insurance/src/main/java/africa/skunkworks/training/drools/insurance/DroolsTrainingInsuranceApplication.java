e8996d3ecc2766f0737611784b1a58a86c987ea7
Guambino <Ivan.Guambe@fnb.co.mz>
2024-01-29 17:33:50 +0200

    Configuration added

---
+++ Course Material/labs/drools-training-insurance/src/main/java/africa/skunkworks/training/drools/insurance/DroolsTrainingInsuranceApplication.java
@@ -0,0 +1,13 @@
+package africa.skunkworks.training.drools.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsTrainingInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsTrainingInsuranceApplication.class, args);
+	}
+
+}
