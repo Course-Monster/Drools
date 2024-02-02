9313f9cb77448d8c63ed2aa20efbc925d5f0d8e2
Guambino <Ivan.Guambe@fnb.co.mz>
2024-02-02 16:07:10 +0200

    Lesson 4 demo 2 done

---
+++ Course Material/labs/drools-training-insurance/drools-training-insurance-rule/src/main/java/africa/skunkworks/training/drools/insurance/rule/lesson04/impl/ClaimValidationLesson04Demo02.java
@@ -0,0 +1,24 @@
+package africa.skunkworks.training.drools.insurance.rule.lesson04.impl;
+
+import africa.skunkworks.training.drools.insurance.domain.dto.ClaimDto;
+import africa.skunkworks.training.drools.insurance.domain.dto.PolicyDto;
+import africa.skunkworks.training.drools.insurance.rule.AbstractValidateApplication;
+import africa.skunkworks.training.drools.insurance.rule.ClaimValidation;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("claimValidationLesson04Demo02")
+public class ClaimValidationLesson04Demo02 extends AbstractValidateApplication implements ClaimValidation {
+
+    @Autowired
+    public ClaimValidationLesson04Demo02(@Qualifier("kieContainerLesson04demo02") KieContainer kieContainer) {
+        super(kieContainer);
+    }
+
+    @Override
+    public void validateClaim(ClaimDto claimDto, PolicyDto policyDto) {
+        super.validateClaim(claimDto, policyDto);
+    }
+}
