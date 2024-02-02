83af1b7771c0b86fe83b61d048848d365c515af8
Guambino <Ivan.Guambe@fnb.co.mz>
2024-02-02 15:51:35 +0200

    Fixed the issue

--- Course Material/labs/drools-training-insurance/drools-training-insurance-rule/src/test/java/africa/skunkworks/training/drools/insurance/rule/lesson04/ClaimValidationLesson04Demo01Test.java
+++ Course Material/labs/drools-training-insurance/drools-training-insurance-rule/src/test/java/africa/skunkworks/training/drools/insurance/rule/lesson04/ClaimValidationLesson04Demo01Test.java
@@ -22,15 +22,18 @@ public class ClaimValidationLesson04Demo01Test extends AbstractRuleTest {
 
     @Test
     public void validateClaim(){
+        //rule "Valid Claims"
         ClaimDto claimDto = getClaim("John Smith","P-101", LocalDate.now(),12399D);
         claimDto.setStatus(Status.PENDING);
 
-        PolicyDto policyDto = getPolicy("P-102","John Smith",130,130000,"Paid");
+        assertEquals(Status.PENDING, claimDto.getStatus());
+
+        PolicyDto policyDto = getPolicy("P-101","John Smith",130,130000,"Paid");
 
         claimValidation.validateClaim(claimDto, policyDto);
         assertNotNull(claimDto.getStatus());
         assertNotNull(claimDto.getReason());
-        assertEquals(Status.APPROVED, claimDto.getStatus());
+        assertEquals(Status.VALID, claimDto.getStatus());
 
         System.out.println("++++++++++++++++++++++++++++++++++++++++ SCENARIO 1 ++++++++++++++++++++++++++++++++++++++++++++++");
         System.out.println(claimDto.getReason());
@@ -38,6 +41,7 @@ public class ClaimValidationLesson04Demo01Test extends AbstractRuleTest {
 
     @Test
     public void validateClaimPayoutIssue(){
+        //rule "Issue Payout"
         ClaimDto claimDto = getClaim("John Smith","P-101", LocalDate.now(),12399D);
         claimDto.setStatus(Status.APPROVED);
 
@@ -46,25 +50,44 @@ public class ClaimValidationLesson04Demo01Test extends AbstractRuleTest {
         claimValidation.validateClaim(claimDto, policyDto);
         assertNotNull(claimDto.getStatus());
         assertNotNull(claimDto.getReason());
+        assertEquals("New Payout: Payout(policyNumber=P-101, claimAmount=12399.0) Issued", claimDto.getReason());
 
         System.out.println("++++++++++++++++++++++++++++++++++++++++ SCENARIO 2 ++++++++++++++++++++++++++++++++++++++++++++++");
         System.out.println(claimDto.getReason());
-
     }
 
     @Test
     public void validateClaimExceedCoverage(){
-        ClaimDto claimDto = getClaim("John Smith","P-101", LocalDate.now(),123990000D);
+        //rule "Invalidate claim that exceeds coverage"
+        ClaimDto claimDto = getClaim("John Smith","P-101", LocalDate.now(),1239900000D);
         claimDto.setStatus(Status.VALID);
 
-        PolicyDto policyDto = getPolicy("P-102","John Smith",130,130000,"Paid");
+        PolicyDto policyDto = getPolicy("P-101","John Smith",130,1300,"Paid");
 
         claimValidation.validateClaim(claimDto, policyDto);
         assertNotNull(claimDto.getStatus());
         assertNotNull(claimDto.getReason());
         assertEquals(Status.DENIED, claimDto.getStatus());
 
-        System.out.println("++++++++++++++++++++++++++++++++++++++++ SCENARIO 1 ++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++ SCENARIO 3 ++++++++++++++++++++++++++++++++++++++++++++++");
         System.out.println(claimDto.getReason());
+    }
+
+    @Test
+    public void validateClaimWithUnpaidPremium(){
+        //rule "Invalidate claim with unpaid premiums"
+        ClaimDto claimDto = getClaim("John Smith","P-101", LocalDate.now(),123D);
+        claimDto.setStatus(Status.VALID);
+
+        PolicyDto policyDto = getPolicy("P-101","John Smith",130,1300000,"Unpaid");
+
+        claimValidation.validateClaim(claimDto, policyDto);
+        assertNotNull(claimDto.getStatus());
+        assertNotNull(claimDto.getReason());
+        assertEquals(Status.DENIED, claimDto.getStatus());
+
+        System.out.println("++++++++++++++++++++++++++++++++++++++++ SCENARIO 4 ++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(claimDto.getReason());
+
     }
 }
